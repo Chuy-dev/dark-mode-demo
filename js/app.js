f9e2c538afb34920c41bee471df279eb2367ba3f
Chuy-Dev <calderasjesus10@gmail.com>
2020-05-27 19:47:50 -0500

    Resuelto el boot de dark mode

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 const checkbox = document.querySelector('#checkbox')
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  checkbox.setAttribute('checked', true)
+  checkbox.setAttribute('checked', true);
+  console.log("is dark");
+}else{
+  console.log("is light");
 }
 
 //document.body.style.setProperty('--cardGray', 'blue');
 
-checkbox.addEventListener('change', function (event) {
+checkbox.addEventListener('change', function(event) {
   if (this.checked) {
-    document.body.classList.remove('is-light-mode')
-    document.body.classList.add('is-dark-mode')
+    document.body.classList.remove('is-light-mode');
+    document.body.classList.add('is-dark-mode');
+    console.log("is dark");
   } else {
     document.body.classList.remove('is-dark-mode');
-    document.body.classList.add('is-light-mode')
+    document.body.classList.add('is-light-mode');
+    console.log("is light");
   }
-})
+});
